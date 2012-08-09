44aea696c55bf5f02547bf05464499d22cbbf4be
Brian Low <github@brianlow.com>
2012-08-09 03:03:42 -0600

    newlines?

--- src/Global.asax.cs
+++ src/Global.asax.cs
@@ -44,7 +44,7 @@ namespace SimpleBoard
             {
                 new LogEvent("Adding messages..").Raise();
                 var initialMessagesFile = EmbeddedResource.Get("InitialMessages.json");
-                var initialMessages = initialMessagesFile.Split(new[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+                var initialMessages = initialMessagesFile.Split(new[] {"\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
                 new LogEvent("Adding " + initialMessages.Count() + "messages..").Raise();
                 initialMessages.ForEach(messageStore.Add);
             }
